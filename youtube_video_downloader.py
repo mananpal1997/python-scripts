c8ce2b1eea32131fb82d1ed58b0f2fdeb495f58a
mananpal1997 <brunodicosta@gmail.com>
2016-03-26 23:59:34 +0530

    Download YouTube videos/playlists
    Just copy the YouTube link and open the app. It gets the link itself
    from the clipboard.

--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -1,50 +1,48 @@
 import pafy
-from Tkinter import Tk
-r = Tk()
-r.withdraw()
+import pyperclip
 flag=0
-result=r.clipboard_get()
-type=raw_input("Hit 1 for video,2 for playlist : ")
+result = pyperclip.paste()
+type=input("Hit 1 for video,2 for playlist : ")
 type=int(type)
-qual=raw_input("Hit 1 for best clarity, 2 for worst, 3 for other : ")
+qual=input("Hit 1 for best clarity, 2 for worst, 3 for other : ")
 qual=int(qual)
-if qual==3:
+if(qual==3):
 	flag=1
 c=0
-if type==1:
+if(type==1):
 	url = result
 	video = pafy.new(url)
 	best = video.streams
-	for b in best:
-		print str(c)+str(b)
-		c+=1;
-	if flag==1:
-		index=raw_input("Enter index : ")
+	if(flag==1):
+                for b in best:
+                        print(str(c)+str(b))
+                        c+=1
+		index=input("Enter index : ")
 		index=int(index)
-	elif qual==2:
+	elif(qual==2):
 		index=c-1
-	elif qual==1:
+	elif(qual==1):
 		index=0
 		
 	filename = video.streams[index]
-	print filename
-	x=filename.download(filepath='E:/YouTube Videos/'+filename.title + "." + filename.extension)
+	print (filename)
+	x=filename.download(filepath='E:/YouTube/')#+filename.title + "." + filename.extension)
 
 else:
 	url=result
 	video=pafy.get_playlist(url)
-	for i in xrange(1,100):
+	for i in range(1,108):
 		c=0
 		best=video['items'][i]['pafy'].streams
-		for b in best:
-			print str(c)+str(b)
-			c+=1
-		if flag==1:
-			index=raw_input("Enter index : ")
+		if(flag==1):
+                        for b in best:
+                                print (str(c)+str(b))
+                                c+=1
+			index=input("Enter index : ")
 			index=int(index)
-		elif qual==2:
+		elif(qual==2):
 			index=c-1
-		elif qual==1:
+		elif(qual==1):
 			index=0
 		filename=video['items'][i]['pafy'].streams[index]
-		x=filename.download(filepath="E:/YouTube Videos")
+		x=filename.download(filepath="E:/YouTube/")
